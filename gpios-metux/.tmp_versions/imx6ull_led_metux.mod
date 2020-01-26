04b30e55d0c5f5fce854c3a3f649902318d57ff7
wanyisq <wanyisq@163.com>
2020-01-26 17:04:25 +0800

    use pinctr device tree and metux

---
+++ gpios-metux/.tmp_versions/imx6ull_led_metux.mod
@@ -0,0 +1,3 @@
+/media/psf/qemu-source/device-tree/linux-dts/gpios-metux/imx6ull_led_metux.ko
+/media/psf/qemu-source/device-tree/linux-dts/gpios-metux/imx6ull_led_metux.o
+
---
+++ gpios-metux/led_test/led_test_metux.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#include <string.h>
+
+#define GPIOS 2
+
+static char userdata[] = {"user data"};
+
+int main(int argc , char **argv){
+	int fd,ret;
+	char *filename;
+	unsigned char databuf[1];
+	unsigned char cnt = 0;
+
+	if(argc != 3){
+		printf("error uasge\n");
+		return -1;
+	}
+
+	filename = argv[1];
+	printf("input file name %s\n", filename);
+	fd = open(filename, O_RDWR);
+	if(fd < 0){
+		printf("can't open file %s\n", filename);
+		return -1;
+	}
+	databuf[0] = atoi(argv[2]);
+	printf("input cmd is %d\n", databuf[0]);
+
+	ret = write(fd, databuf, sizeof(databuf));
+	if(ret < 0){
+		printf("write cmd failed\n");
+	}
+
+	while(1){
+		sleep(5);
+		cnt++;
+		printf("app run times %d\n", cnt);
+		if(cnt > 5){
+			break;
+		}
+	}
+
+	ret = close(fd);
+	if(ret < 0){
+		printf("can't close file %s\n", filename);
+		return -1;
+	}
+	printf("led test end\n");
+	return 0;
+}
